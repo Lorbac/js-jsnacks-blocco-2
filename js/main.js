c5bb0e33b887712ab8c4379dd5f76e6e2981a9d0
Lorbac <baccini.lorenzo@gmail.com>
2021-07-21 16:24:37 +0200

    Primi 3 Snacks

---
+++ js/main.js
@@ -0,0 +1,65 @@
+//Snack 1 --------------------------------------------
+
+
+
+// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+
+// var num = parseInt(prompt("Inserisci un numero"));
+
+// if (num % 2 == 0)
+// {
+//     console.log(num);
+// }
+// else if (num % 2 != 0)
+// {
+//     console.log(num + 1);
+// }
+
+
+
+//Snack 2 --------------------------------------------
+
+
+
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
+
+// var nomi = ["Giovanni", "Andrea", "Alessio", "Marco", "Mauro" ];
+// var cognomi = ["Rossi", "Verdi", "Esposito", "Gialli", "Neri" ];
+
+// for ( i=0 ; i < 5 ; i++)
+// {
+//     nomiRandom = Math.floor(Math.random()*nomi.length);
+//     cognomiRandom = Math.floor(Math.random()*cognomi.length);
+//     console.log((nomi[nomiRandom]) + " " + (cognomi[cognomiRandom]));
+// }
+
+
+
+//Snack 3 --------------------------------------------
+
+
+
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+// var num = [1,2,3,4,5,6,7,8,9,10];
+// var sommaDispari = 0;
+
+// for ( i=0 ; i < num.length ; i++ )
+// {
+//     if (num[i] % 2 != 0)
+//     {
+//         sommaDispari += num[i];
+//     }
+// }
+
+// console.log(sommaDispari);
+
+
+
+//Snack 4 --------------------------------------------
+
+
+
+// Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+
